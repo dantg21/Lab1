f047c5bd35eac9c2ee211b6b6a2c6b127b6abae2
George <Dantg21@hanover.edu>
2019-03-15 14:07:38 -0400

    Test Refactoring

--- Lab4/js/test_score.spec.js
+++ Lab4/js/test_score.spec.js
@@ -4,12 +4,19 @@ import Score from '../js/score.js';
 
 describe('Score instances', () => {
 
+	let score;
+	beforeEach(() => {score = new Score(); });
+
 	it('Start with all counts set to zero', () => {
-    		let score = new Score();
-    		expect (score.switchWins).to.equal(0);
-    		expect (score.switchLosses).to.equal(0);
-    		expect (score.stayWins).to.equal(0);
-    		epxect (score.stayLosses).to.equal(0);
+		checkValuesAre(switchWins, switchLosses, stayWins, stayLosses){
+			expect (score.switchWins).to.equal(switchWins);
+    		expect (score.switchLosses).to.equal(switchLosses);
+    		expect (score.stayWins).to.equal(stayWins);
+    		epxect (score.stayLosses).to.equal(stayLosses);
+		}
+
+		checkValuesAre(0, 0, 0, 0);
+
    		});
 
 });
