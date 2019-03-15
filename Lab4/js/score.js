7318d8136bddc8e88a160699e51b8eeb24652ed5
George <Dantg21@hanover.edu>
2019-03-15 13:53:35 -0400

    Add basic Score card. Ref #2

---
+++ Lab4/js/score.js
@@ -0,0 +1,8 @@
+export default class Score {
+	constructor(switchWins, switchLosses, stayWins, stayLosses){
+		this.switchWins=0;
+		this.switchLosses=0;
+		this.stayWins=0;
+		this.stayLosses=0;
+	}
+}
--- Lab4/test/test_score.spec.js
+++ Lab4/test/test_score.spec.js
@@ -6,13 +6,6 @@ describe('Score instances', () => {
 
 	it('Start with all counts set to zero', () => {
     		let score = new Score();
-    		expect (score.switchWins).to.equal(0);
-    		expect (score.switchLosses).to.equal(0);
-    		expect (score.stayWins).to.equal(0);
-    		epxect (score.stayLosses).to.equal(0);
    		});
 
 });
-
-
-
